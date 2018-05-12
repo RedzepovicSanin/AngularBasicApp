958551dfc9286a9dd9491971db487421e37e00c3
Dev-Sanin <sanin.redzepovic@gmail.com>
2018-05-12 19:14:24 +0200

    finished small website

---
+++ src/app/products/product.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { SharedModule } from './../shared/shared.module';
+import { ProductRoutingModule } from './product-routing.module';
+
+import { ProductListComponent } from './product-list.component';
+import { ProductDetailComponent } from './product-detail.component';
+import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
+
+import { ProductService } from './product.service';
+import { ProductGuardService } from './product-guard.service';
+
+@NgModule({
+  imports: [
+    SharedModule,
+    ProductRoutingModule
+  ],
+  declarations: [
+    ProductListComponent,
+    ProductDetailComponent,
+    ConvertToSpacesPipe
+  ],
+  providers: [
+    ProductService,
+    ProductGuardService
+  ]
+})
+export class ProductModule { }
